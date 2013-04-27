7a9eb1119cc79fd610f751904cdc3c95426bfa22
Örs Kovács <orskovacs@hotmail.com>
2013-04-27 18:40:03 +0200

    start

---
+++ WP8HF/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WP8HF.Resources;
+
+namespace WP8HF
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
